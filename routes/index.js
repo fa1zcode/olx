6688310b7daf08d0ee621d3b031f74ab278a4ac6
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-03-13 22:06:45 +0700

    sell feature done

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,14 @@ const saltRounds = 10;
 /* GET home page. */
 module.exports = function (db) {
   router.get("/", function (req, res, next) {
-    
-
     const url =
       req.url == "/"
         ? "/ads?page=1&sortBy=id&sortMode=asc"
         : req.url.replace("/", "/ads");
 
     const params = [];
+
+    params.push("approved is true");
 
     if (req.query.title) {
       params.push(`title ilike '%${req.query.title}%'`);
@@ -55,7 +55,7 @@ module.exports = function (db) {
           if (err) return res.send(err);
           db.query("select * from users order by id", (err, users) => {
             if (err) return res.send(err);
-            console.log(data.rows)
+
             res.render("index", {
               data: data.rows,
               page,
@@ -72,6 +72,121 @@ module.exports = function (db) {
         });
       });
     });
+  });
+
+  router.get("/sell", helpers.isLoggedIn, function (req, res) {
+    db.query("select * from categories order by id", (err, categories) => {
+      if (err) return res.send(err);
+
+      res.render("sell", {
+        user: req.session.user,
+        data: {},
+        categories: categories.rows,
+        path: req.originalUrl,
+      });
+    });
+  });
+
+  router.post("/sell", helpers.isLoggedIn, function (req, res) {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      db.query(
+        "insert into ads(title, description, category, seller, price, approved, pictures) values ($1, $2, $3, $4, $5, $6, $7)",
+        [
+          req.body.title,
+          req.body.description,
+          Number(req.body.category),
+          req.session.user.id,
+          Number(req.body.price),
+          false,
+          [],
+        ],
+        (err) => {
+          if (err) {
+            console.log(err);
+            req.flash("successMessage", `gagal bikin ads`);
+            return res.redirect("/");
+          }
+          res.redirect("/profile");
+        }
+      );
+    } else {
+      const fileNames = [];
+      if (req.files.picture.length > 1) {
+        req.files.picture.forEach((file) => {
+          const fileName = `${Date.now()}-${file.name}`;
+          const uploadPath = path.join(
+            __dirname,
+            "..",
+            "public",
+            "images",
+            "ads",
+            fileName
+          );
+          fileNames.push(fileName);
+          // Use the mv() method to place the file somewhere on your server
+          file.mv(uploadPath, function (err) {
+            if (err) console.log(err);
+          });
+        });
+        db.query(
+          "insert into ads(title, description, category, seller, price, approved, pictures) values ($1, $2, $3, $4, $5, $6, $7)",
+          [
+            req.body.title,
+            req.body.description,
+            Number(req.body.category),
+            req.session.user.id,
+            Number(req.body.price),
+            false,
+            fileNames,
+          ],
+          (err) => {
+            if (err) {
+              console.log(err);
+              req.flash("successMessage", `gagal bikin ads plus picture`);
+              return res.redirect("/");
+            }
+            res.redirect("/profile");
+          }
+        );
+      } else {
+        const file = req.files.picture;
+        const fileName = `${Date.now()}-${file.name}`;
+        const uploadPath = path.join(
+          __dirname,
+          "..",
+          "public",
+          "images",
+          "ads",
+          fileName
+        );
+
+        // Use the mv() method to place the file somewhere on your server
+        file.mv(uploadPath, function (err) {
+          if (err) return res.status(500).send(err);
+          fileNames.push(fileName);
+          db.query(
+            "insert into ads(title, description, category, seller, price, approved, pictures) values ($1, $2, $3, $4, $5, $6, $7)",
+            [
+              req.body.title,
+              req.body.description,
+              Number(req.body.category),
+              req.session.user.id,
+              Number(req.body.price),
+              false,
+              fileNames,
+            ],
+            (err) => {
+              if (err) {
+                console.log(err);
+                req.flash("successMessage", `gagal bikin ads plus pictures`);
+                return res.redirect("/");
+              }
+              res.redirect("/");
+            }
+          );
+        });
+      }
+    }
   });
 
   router.get("/login", function (req, res) {
